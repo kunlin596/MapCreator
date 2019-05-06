828c56463606b98297e49cb85dd71cb67a88910e
Kun Lin <kun.lin.596@gmail.com>
2019-05-06 17:05:28 +0900

    (style) Convert tabs to spaces

--- src/Core/Utility.cpp
+++ src/Core/Utility.cpp
@@ -9,58 +9,58 @@
 
 namespace MapCreator {
 
-	void PrintMessage ( const std::string msg , int status ) {
+    void PrintMessage ( const std::string msg , int status ) {
 
-		using namespace std;
+        using namespace std;
 
-		string m = "[SLAM]";
-		switch ( status ) {
-			case 1:
-				m += " [PROCESSING]  - ";
-				break;
-			case 2:
-				m += " [OK]          - ";
-				break;
-			case 3:
-				m += " [WARNING]     - ";
-				break;
-			case 4:
-				m += " [ERROR]       - ";
-				break;
-			default:
-				break;
-		}
+        string m = "[SLAM]";
+        switch ( status ) {
+            case 1:
+                m += " [PROCESSING]  - ";
+                break;
+            case 2:
+                m += " [OK]          - ";
+                break;
+            case 3:
+                m += " [WARNING]     - ";
+                break;
+            case 4:
+                m += " [ERROR]       - ";
+                break;
+            default:
+                break;
+        }
 
-		cout << m << msg << endl;
-	}
+        cout << m << msg << endl;
+    }
 
-	std::string ConvertConstCStrToStdString ( const unsigned char * c_str , size_t len ) {
+    std::string ConvertConstCStrToStdString ( const unsigned char * c_str , size_t len ) {
 
-		std::string str;
-		str.resize ( len );
-		for ( auto i = 0 ; i < len ; ++i ) {
-			str[ i ] = * ( c_str + i );
-		}
-		return str;
-	}
+        std::string str;
+        str.resize ( len );
+        for ( auto i = 0 ; i < len ; ++i ) {
+            str[ i ] = * ( c_str + i );
+        }
+        return str;
+    }
 
-	glm::mat4 ConvertMat ( const cv::Matx44f & m ) {
+    glm::mat4 ConvertMat ( const cv::Matx44f & m ) {
 
-		return glm::mat4 ( );
-	}
+        return glm::mat4 ( );
+    }
 
 
-	glm::mat4 Convert_OpenCV_Matx44f_To_GLM_mat4 ( const cv::Matx44f & m ) {
+    glm::mat4 Convert_OpenCV_Matx44f_To_GLM_mat4 ( const cv::Matx44f & m ) {
 
-		return glm::make_mat4 ( m.val );
+        return glm::make_mat4 ( m.val );
 
-	}
+    }
 
-	cv::Matx44f Convert_GLM_mat4_To_OpenCV_Matx44f ( const glm::mat4 & m ) {
+    cv::Matx44f Convert_GLM_mat4_To_OpenCV_Matx44f ( const glm::mat4 & m ) {
 
-		return cv::Matx44f ( glm::value_ptr ( m ) );
+        return cv::Matx44f ( glm::value_ptr ( m ) );
 
-	}
+    }
 
 
 }
