a012f4ca391ec70dfabb301e433f786351823878
Kun Lin <kun.lin.596@gmail.com>
2019-05-07 11:11:30 +0900

    (refactor) Convert tabs to spaces

--- include/SLAM/SlamParameters.h
+++ include/SLAM/SlamParameters.h
@@ -13,191 +13,191 @@
 
 namespace MapCreator {
 
-	struct TrackerParameters
-	{
-		struct Consecutive
-		{
-			int   num_ransac_iteration;
-			float threshold_outlier;
-			float threshold_inlier;
+    struct TrackerParameters
+    {
+        struct Consecutive
+        {
+            int   num_ransac_iteration;
+            float threshold_outlier;
+            float threshold_inlier;
 
-			inline Consecutive ( ) :
-					num_ransac_iteration ( 10000 ) ,
-					threshold_outlier ( 0.035f ) ,
-					threshold_inlier ( 0.035f ) { }
+            inline Consecutive ( ) :
+                    num_ransac_iteration ( 10000 ) ,
+                    threshold_outlier ( 0.035f ) ,
+                    threshold_inlier ( 0.035f ) { }
 
-			inline Consecutive ( int num_ransac_iteration ,
-			                          float threshold_outlier ,
-			                          float threshold_inlier ) :
-					num_ransac_iteration ( num_ransac_iteration ) ,
-					threshold_outlier ( threshold_outlier ) ,
-					threshold_inlier ( threshold_inlier ) { }
+            inline Consecutive ( int num_ransac_iteration ,
+                                      float threshold_outlier ,
+                                      float threshold_inlier ) :
+                    num_ransac_iteration ( num_ransac_iteration ) ,
+                    threshold_outlier ( threshold_outlier ) ,
+                    threshold_inlier ( threshold_inlier ) { }
 
-			inline QString Output ( ) const {
+            inline QString Output ( ) const {
 
-				QString res;
-				res.append ( QString ( "TrackerParameters of SLAM :\n" ) );
-				res.append ( QString ( "----------------------------------\n" ) );
-				res.append ( QString ( "Number of RANSAC Iteration : %1\n" ).arg ( QString::number ( num_ransac_iteration ) ) );
-				res.append ( QString ( "Threshold of Outlier       : %1\n" ).arg ( QString::number ( threshold_outlier ) ) );
-				res.append ( QString ( "Threshold of Inlier        : %1\n" ).arg ( QString::number ( threshold_inlier ) ) );
-				res.append ( QString ( "----------------------------------\n" ) );
-				return res;
-			}
+                QString res;
+                res.append ( QString ( "TrackerParameters of SLAM :\n" ) );
+                res.append ( QString ( "----------------------------------\n" ) );
+                res.append ( QString ( "Number of RANSAC Iteration : %1\n" ).arg ( QString::number ( num_ransac_iteration ) ) );
+                res.append ( QString ( "Threshold of Outlier       : %1\n" ).arg ( QString::number ( threshold_outlier ) ) );
+                res.append ( QString ( "Threshold of Inlier        : %1\n" ).arg ( QString::number ( threshold_inlier ) ) );
+                res.append ( QString ( "----------------------------------\n" ) );
+                return res;
+            }
 
-			template < class Archive >
-			void serialize ( Archive & ar , const unsigned int version ) {
+            template < class Archive >
+            void serialize ( Archive & ar , const unsigned int version ) {
 
-				ar & num_ransac_iteration;
-				ar & threshold_outlier;
-				ar & threshold_inlier;
+                ar & num_ransac_iteration;
+                ar & threshold_outlier;
+                ar & threshold_inlier;
 
-			}
+            }
 
-		};
+        };
 
-		struct FixedNumber : public Consecutive
-		{
-			int frame_count;
+        struct FixedNumber : public Consecutive
+        {
+            int frame_count;
 
-			FixedNumber ( ) :
-					Consecutive ( ) ,
-					frame_count ( 1 ) { }
+            FixedNumber ( ) :
+                    Consecutive ( ) ,
+                    frame_count ( 1 ) { }
 
-			explicit FixedNumber ( int frame_count ) :
-					Consecutive ( ) ,
-					frame_count ( frame_count ) { }
+            explicit FixedNumber ( int frame_count ) :
+                    Consecutive ( ) ,
+                    frame_count ( frame_count ) { }
 
-			inline QString Output ( ) const {
+            inline QString Output ( ) const {
 
-				QString res;
-				res.append ( QString ( "TrackerParameters of Fixed Frame Count Tracking :\n" ) );
-				res.append ( QString ( "----------------------------------\n" ) );
-				res.append ( QString ( "Number of RANSAC Iteration : %1\n" ).arg ( QString::number ( num_ransac_iteration ) ) );
-				res.append ( QString ( "Threshold of Outlier       : %1\n" ).arg ( QString::number ( threshold_outlier ) ) );
-				res.append ( QString ( "Threshold of Inlier        : %1\n" ).arg ( QString::number ( threshold_inlier ) ) );
-				res.append ( QString ( "Number of frames           : %1\n" ).arg ( QString::number ( frame_count ) ) );
-				res.append ( QString ( "----------------------------------\n" ) );
+                QString res;
+                res.append ( QString ( "TrackerParameters of Fixed Frame Count Tracking :\n" ) );
+                res.append ( QString ( "----------------------------------\n" ) );
+                res.append ( QString ( "Number of RANSAC Iteration : %1\n" ).arg ( QString::number ( num_ransac_iteration ) ) );
+                res.append ( QString ( "Threshold of Outlier       : %1\n" ).arg ( QString::number ( threshold_outlier ) ) );
+                res.append ( QString ( "Threshold of Inlier        : %1\n" ).arg ( QString::number ( threshold_inlier ) ) );
+                res.append ( QString ( "Number of frames           : %1\n" ).arg ( QString::number ( frame_count ) ) );
+                res.append ( QString ( "----------------------------------\n" ) );
 
-				return res;
-			}
+                return res;
+            }
 
-			template < class Archive >
-			void serialize ( Archive & ar , const unsigned int version ) {
+            template < class Archive >
+            void serialize ( Archive & ar , const unsigned int version ) {
 
-				ar & boost::serialization::base_object < Consecutive > ( * this );
-				ar & frame_count;
-			}
+                ar & boost::serialization::base_object < Consecutive > ( * this );
+                ar & frame_count;
+            }
 
-		};
+        };
 
-		struct KeyFrameOnly : public Consecutive
-		{
-			int   num_inliers;
-			float threshold_1st_component_contribution;
-			float threshold_1st_component_variance;
-			float threshold_2nd_component_variance;
-			float threshold_3rd_component_variance;
+        struct KeyFrameOnly : public Consecutive
+        {
+            int   num_inliers;
+            float threshold_1st_component_contribution;
+            float threshold_1st_component_variance;
+            float threshold_2nd_component_variance;
+            float threshold_3rd_component_variance;
 
-			inline KeyFrameOnly ( ) :
-					Consecutive ( ) ,
-					num_inliers ( 3 ) ,
-					threshold_1st_component_contribution ( 0.85f ) ,
-					threshold_1st_component_variance ( 0.1f ) ,
-					threshold_2nd_component_variance ( 0.05f ) ,
-					threshold_3rd_component_variance ( 0.0f ) { }
+            inline KeyFrameOnly ( ) :
+                    Consecutive ( ) ,
+                    num_inliers ( 3 ) ,
+                    threshold_1st_component_contribution ( 0.85f ) ,
+                    threshold_1st_component_variance ( 0.1f ) ,
+                    threshold_2nd_component_variance ( 0.05f ) ,
+                    threshold_3rd_component_variance ( 0.0f ) { }
 
-			inline KeyFrameOnly ( int num_inliers ,
-			                             float threshold_1st_component_contribution ,
-			                             float threshold_1st_component_variance ,
-			                             float threshold_2nd_component_variance ,
-			                             float threshold_3rd_component_variance ) :
-					Consecutive ( ) ,
-					num_inliers ( num_inliers ) ,
-					threshold_1st_component_contribution ( threshold_1st_component_contribution ) ,
-					threshold_1st_component_variance ( threshold_1st_component_variance ) ,
-					threshold_2nd_component_variance ( threshold_2nd_component_variance ) ,
-					threshold_3rd_component_variance ( threshold_3rd_component_variance ) { }
+            inline KeyFrameOnly ( int num_inliers ,
+                                         float threshold_1st_component_contribution ,
+                                         float threshold_1st_component_variance ,
+                                         float threshold_2nd_component_variance ,
+                                         float threshold_3rd_component_variance ) :
+                    Consecutive ( ) ,
+                    num_inliers ( num_inliers ) ,
+                    threshold_1st_component_contribution ( threshold_1st_component_contribution ) ,
+                    threshold_1st_component_variance ( threshold_1st_component_variance ) ,
+                    threshold_2nd_component_variance ( threshold_2nd_component_variance ) ,
+                    threshold_3rd_component_variance ( threshold_3rd_component_variance ) { }
 
-			inline QString Output ( ) const {
+            inline QString Output ( ) const {
 
-				QString res;
-				res.append ( QString ( "TrackerParameters of PCA Key Frame Tracking :\n" ) );
-				res.append ( QString ( "----------------------------------\n" ) );
-				res.append ( QString ( "Number of RANSAC Iteration : %1\n" ).arg ( QString::number ( num_ransac_iteration ) ) );
-				res.append ( QString ( "Threshold of Outlier       : %1\n" ).arg ( QString::number ( threshold_outlier ) ) );
-				res.append ( QString ( "Threshold of Inlier        : %1\n\n" ).arg ( QString::number ( threshold_inlier ) ) );
-				res.append ( QString ( "Number of inliers          : %1\n" ).arg ( QString::number ( num_inliers ) ) );
-				res.append ( QString ( "1st PC contribution        : %1\n" ).arg (
-						QString::number ( threshold_1st_component_contribution ) ) );
-				res.append ( QString ( "1st PC eigenvalue          : %1\n" ).arg (
-						QString::number ( threshold_1st_component_variance ) ) );
-				res.append ( QString ( "2nd PC eigenvalue          : %1\n" ).arg (
-						QString::number ( threshold_2nd_component_variance ) ) );
-				res.append ( QString ( "3rd PC eigenvalue          : %1\n" ).arg (
-						QString::number ( threshold_3rd_component_variance ) ) );
-				res.append ( QString ( "----------------------------------\n" ) );
+                QString res;
+                res.append ( QString ( "TrackerParameters of PCA Key Frame Tracking :\n" ) );
+                res.append ( QString ( "----------------------------------\n" ) );
+                res.append ( QString ( "Number of RANSAC Iteration : %1\n" ).arg ( QString::number ( num_ransac_iteration ) ) );
+                res.append ( QString ( "Threshold of Outlier       : %1\n" ).arg ( QString::number ( threshold_outlier ) ) );
+                res.append ( QString ( "Threshold of Inlier        : %1\n\n" ).arg ( QString::number ( threshold_inlier ) ) );
+                res.append ( QString ( "Number of inliers          : %1\n" ).arg ( QString::number ( num_inliers ) ) );
+                res.append ( QString ( "1st PC contribution        : %1\n" ).arg (
+                        QString::number ( threshold_1st_component_contribution ) ) );
+                res.append ( QString ( "1st PC eigenvalue          : %1\n" ).arg (
+                        QString::number ( threshold_1st_component_variance ) ) );
+                res.append ( QString ( "2nd PC eigenvalue          : %1\n" ).arg (
+                        QString::number ( threshold_2nd_component_variance ) ) );
+                res.append ( QString ( "3rd PC eigenvalue          : %1\n" ).arg (
+                        QString::number ( threshold_3rd_component_variance ) ) );
+                res.append ( QString ( "----------------------------------\n" ) );
 
-				return res;
-			}
+                return res;
+            }
 
-			template < class Archive >
-			void serialize ( Archive & ar , const unsigned int version ) {
+            template < class Archive >
+            void serialize ( Archive & ar , const unsigned int version ) {
 
-				ar & boost::serialization::base_object < Consecutive > ( * this );
-				ar & num_inliers;
-				ar & threshold_1st_component_contribution;
-				ar & threshold_1st_component_variance;
-				ar & threshold_2nd_component_variance;
-				ar & threshold_3rd_component_variance;
-			}
-		};
+                ar & boost::serialization::base_object < Consecutive > ( * this );
+                ar & num_inliers;
+                ar & threshold_1st_component_contribution;
+                ar & threshold_1st_component_variance;
+                ar & threshold_2nd_component_variance;
+                ar & threshold_3rd_component_variance;
+            }
+        };
 
-		TrackerParameters ( ) : type_ ( TrackingType::Unknown ) { }
+        TrackerParameters ( ) : type_ ( TrackingType::Unknown ) { }
 
-		TrackerParameters ( const Consecutive & paramsConsectutive , const FixedNumber & paramsFixedNumber ,
-		          const KeyFrameOnly & paramsKeyFramesOnly ) :
-				type_ ( TrackingType::Unknown ) ,
-				paramsConsectutive ( paramsConsectutive ) ,
-				paramsFixedNumber ( paramsFixedNumber ) ,
-				paramsKeyFramesOnly ( paramsKeyFramesOnly ) { }
+        TrackerParameters ( const Consecutive & paramsConsectutive , const FixedNumber & paramsFixedNumber ,
+                  const KeyFrameOnly & paramsKeyFramesOnly ) :
+                type_ ( TrackingType::Unknown ) ,
+                paramsConsectutive ( paramsConsectutive ) ,
+                paramsFixedNumber ( paramsFixedNumber ) ,
+                paramsKeyFramesOnly ( paramsKeyFramesOnly ) { }
 
-		TrackingType            type_;
-		bool                    use_bundle_adjustment_;
-		Consecutive        paramsConsectutive;
-		FixedNumber paramsFixedNumber;
-		KeyFrameOnly     paramsKeyFramesOnly;
+        TrackingType            type_;
+        bool                    use_bundle_adjustment_;
+        Consecutive        paramsConsectutive;
+        FixedNumber paramsFixedNumber;
+        KeyFrameOnly     paramsKeyFramesOnly;
 
-		TrackingType GetType ( ) const { return type_; }
+        TrackingType GetType ( ) const { return type_; }
 
-		void UseBundleAdjustment ( bool use ) { use_bundle_adjustment_ = use; }
+        void UseBundleAdjustment ( bool use ) { use_bundle_adjustment_ = use; }
 
-		bool IsUsingBundleAdjustment ( ) const { return use_bundle_adjustment_; };
+        bool IsUsingBundleAdjustment ( ) const { return use_bundle_adjustment_; };
 
-		inline void SetOptionsType ( const TrackingType & type ) { type_ = type; }
+        inline void SetOptionsType ( const TrackingType & type ) { type_ = type; }
 
-		template < typename TrackingOptionsType >
-		TrackingOptionsType GetParameters ( );
+        template < typename TrackingOptionsType >
+        TrackingOptionsType GetParameters ( );
 
-		template < typename Archive >
-		void serialize ( Archive & ar , const unsigned int version ) {
+        template < typename Archive >
+        void serialize ( Archive & ar , const unsigned int version ) {
 
-			ar & type_;
-			ar & use_bundle_adjustment_;
-			ar & paramsConsectutive;
-			ar & paramsFixedNumber;
-			ar & paramsKeyFramesOnly;
-		}
-	};
+            ar & type_;
+            ar & use_bundle_adjustment_;
+            ar & paramsConsectutive;
+            ar & paramsFixedNumber;
+            ar & paramsKeyFramesOnly;
+        }
+    };
 
-	template < >
-	TrackerParameters::Consecutive TrackerParameters::GetParameters ( );
+    template < >
+    TrackerParameters::Consecutive TrackerParameters::GetParameters ( );
 
-	template < >
-	TrackerParameters::FixedNumber TrackerParameters::GetParameters ( );
+    template < >
+    TrackerParameters::FixedNumber TrackerParameters::GetParameters ( );
 
-	template < >
-	TrackerParameters::KeyFrameOnly TrackerParameters::GetParameters ( );
+    template < >
+    TrackerParameters::KeyFrameOnly TrackerParameters::GetParameters ( );
 }
 
 #endif //MAPCREATOR_SLAMPARAMETERS_H
