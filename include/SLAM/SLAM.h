9d66ac4cb14f5c1aa8883f4cf36312b286f5010d
Kun Lin <kun.lin.596@gmail.com>
2019-05-15 08:26:24 +0900

    (style) Foramt SLAM.h

--- include/SLAM/SLAM.h
+++ include/SLAM/SLAM.h
@@ -6,37 +6,35 @@
 
 namespace MapCreator {
 
-	using Matrices = std::vector < glm::mat4 >;
-	using KeyPoints = std::vector < cv::KeyPoint >;
-	using Points = std::vector < cv::Point3f >;
-	using CorrespondingPointsPair = std::pair < Points , Points >;
-	using InlierPoints = Points;
-	using PointImage = cv::Mat_ < cv::Vec3f >;
-	using ColorImage = cv::Mat_ < cv::Vec3b >;
-	using DepthImage = cv::Mat_ < ushort >;
+  using Matrices = std::vector < glm::mat4 >;
+  using KeyPoints = std::vector < cv::KeyPoint >;
+  using Points = std::vector < cv::Point3f >;
+  using CorrespondingPointsPair = std::pair < Points , Points >;
+  using InlierPoints = Points;
+  using PointImage = cv::Mat_ < cv::Vec3f >;
+  using ColorImage = cv::Mat_ < cv::Vec3b >;
+  using DepthImage = cv::Mat_ < ushort >;
 
-	enum TrackingType
-	{
-		Unknown  = -1 ,
-		Consecutive = 0 ,
-		KeyFrameOnly ,
-		FixedNumber
-	};
+  enum TrackingType {
+    Unknown = -1,
+    Consecutive = 0,
+    KeyFrameOnly,
+    FixedNumber
+  };
 
-	using PointPair = std::pair < glm::vec3 , glm::vec3 >;
+  using PointPair = std::pair < glm::vec3 , glm::vec3 >;
 
-	using ScreenPoint = cv::Point2f;
-	using WorldPoint = cv::Point3f;
+  using ScreenPoint = cv::Point2f;
+  using WorldPoint = cv::Point3f;
 
-	struct MatchedPoint
-	{
-		int         id;
-		ScreenPoint screen_point;
-		WorldPoint  world_point;
-	};
+  struct MatchedPoint
+  {
+      int         id;
+      ScreenPoint screen_point;
+      WorldPoint  world_point;
+  };
 
-	using MatchedPoints = std::vector < MatchedPoint >;
-
-}
+  using MatchedPoints = std::vector < MatchedPoint >;
+  }  // namespace MapCreator
 
 #endif
