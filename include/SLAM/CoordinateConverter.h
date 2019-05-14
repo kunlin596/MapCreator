30768b7e6d133be2ad8cdd414d7e566d9519664d
Kun Lin <kun.lin.596@gmail.com>
2019-05-14 14:18:44 +0900

    (refactor) Refactor CoordinateConverter.h

--- include/SLAM/CoordinateConverter.h
+++ include/SLAM/CoordinateConverter.h
@@ -1,99 +1,54 @@
-//
-// Created by LinKun on 11/4/15.
-//
-
 #ifndef MAPCREATOR_COORDINATECONVERTER_H
 #define MAPCREATOR_COORDINATECONVERTER_H
 
 #include <opencv2/opencv.hpp>
 
-#include <fstream>
-
-#include "Core/Serialize.h"
-#include "SLAM/Calibrator.h"
+#include "SLAM/Calibrator.h" // for InternalCalibrationData
 
 namespace MapCreator {
 
-class CoordinateConverter {
+class AbstractCoordinateConverter {
  public:
-  virtual ScreenPoint WorldToScreen(const WorldPoint& world_point) const {
-    return ScreenPoint();
-  };
 
-  virtual WorldPoint ScreenToWorld(const ScreenPoint& screen_point,
-                                   ushort const depth) const {
-    return WorldPoint();
-  };
-
-  struct XtionFrameProperty {
-    static const float kXtionHorizontalFOV;
-    static const float kXtionVerticalFOV;
-    static const float kXtionWidth;
-    static const float kXtionHeight;
-  };
+  virtual cv::Mat Project(const cv::Mat& image) = 0;
+  virtual cv::Mat Unproject(const cv::Mat& image) = 0;
 };
 
-class XtionCoordinateConverter : public CoordinateConverter {
+class CoordinateConverter : public AbstractCoordinateConverter {
  public:
-  XtionCoordinateConverter()
-      : universal_xz_factor_(
-            GetXtionDepthFactor(XtionFrameProperty::kXtionHorizontalFOV)),
-        universal_yz_factor_(
-            GetXtionDepthFactor(XtionFrameProperty::kXtionVerticalFOV)) {}
-
-  ~XtionCoordinateConverter() = default;
-
-  ScreenPoint WorldToScreen(WorldPoint const& world_point) const override;
-
-  WorldPoint ScreenToWorld(ScreenPoint const& screen_point,
-                           ushort const depth) const override;
+  CoordinateConverter();
+  ~CoordinateConverter() = default;
+  cv::Mat Project(cv::Mat const& image) const override;
+  cv::Mat Unproject(cv::Mat const& image) const override;
 
  private:
-  float GetXtionDepthFactor(float fov) { return tanf(fov / 2) * 2; }
+  float GetDepthFactor(float fov) { return tanf(fov / 2) * 2; }
+
+  float Init();
 
   float universal_xz_factor_;
   float universal_yz_factor_;
 };
 
-class AistCoordinateConverter : public CoordinateConverter {
+class CalibratedCoordinateConverter : public AbstractCoordinateConverter {
  public:
-  AistCoordinateConverter() = default;
-  ~AistCoordinateConverter() = default;
-  inline AistCoordinateConverter(std::string const& file_name) {
-    internal_calibration_info_ = InternalCalibrationReader::Read(file_name);
-  }
+  CalibratedCoordinateConverter() = default;
+  ~CalibratedCoordinateConverter() = default;
 
-  ScreenPoint WorldToScreen(WorldPoint const& world_point) const override;
+  cv::Mat Project(cv::Mat const& image) const override;
+  cv::Mat Unproject(cv::Mat const& image) const override;
 
-  WorldPoint ScreenToWorld(ScreenPoint const& screen_point,
-                           ushort const depth) const override;
+  // TODO: Pass in JSON object
+  // inline LoadCalibrationData(std::string const& file_name) {
+  //   internal_calibration_data_ = InternalCalibrationReader::Read(file_name);
+  // }
 
  private:
-  float NthDegreeEquation(
-      const InternalCalibrationInfo::CoefficientsVector& coef, float x) const {
-    float y = 0;
-    float xx = 1.0f;
+  float NthDegreeEquation(std::vector<float> const& coef, float x) const;
+  float CorrectDepth(float depth) const;
+  float CorrectDistortion(int row, int col, float depth) const;
 
-    for (const auto& c : coef) {
-      y += c * xx;
-      xx *= x;
-    }
-
-    return y;
-  }
-
-  float CorrectDepth(float depth) const {
-    const auto& coef = internal_calibration_info_.global_calibration_vector;
-    return coef.empty() ? depth : depth * NthDegreeEquation(coef, depth);
-  }
-
-  float CorrectDistortion(int row, int col, float depth) const {
-    const auto& coef =
-        internal_calibration_info_.local_calibration_table[row][col];
-    return coef.empty() ? 0.0f : depth * NthDegreeEquation(coef, depth);
-  }
-
-  InternalCalibrationInfo internal_calibration_info_;
+  InternalCalibrationData internal_calibration_data_;
 };
 };  // namespace MapCreator
 
