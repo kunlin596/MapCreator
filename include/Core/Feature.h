66f722641dda7b63f7772c1f405fdbf1d1bc02db
Kun Lin <kun.lin.596@gmail.com>
2019-05-15 08:24:02 +0900

    (style) Foramt Feature

--- include/Core/Feature.h
+++ include/Core/Feature.h
@@ -1,82 +1,72 @@
-//
-// Created by LinKun on 9/12/15.
-//
-
 #ifndef MAPCREATOR_FEATURE_H
 #define MAPCREATOR_FEATURE_H
 
 #include "Serialize.h"
 
-#include <opencv2/opencv.hpp>
 #include <opencv2/features2d.hpp>
+#include <opencv2/opencv.hpp>
 
 #ifdef ENABLE_OPENCV_CONTRIB
 #include <opencv2/xfeatures2d.hpp>
 #endif
 
-#include <fstream>
 #include <QDebug>
+#include <fstream>
 
 namespace MapCreator {
 
-    class Feature
-    {
-    public:
-
-        enum Type
-        {
-            kTypeUnknown = -1 ,     ///< Unknown
-            kTypeORB     = 0 ,      ///< ORB
+class Feature {
+ public:
+  enum Type {
+    kTypeUnknown = -1,  ///< Unknown
+    kTypeORB = 0,       ///< ORB
 #ifdef ENABLE_OPENCV_CONTRIB
-            kTypeFREAK ,            ///< FREAK
-            kTypeSIFT ,             ///< SIFT
-            kTypeSURF ,             ///< SURF
+    kTypeFREAK,         ///< FREAK
+    kTypeSIFT,          ///< SIFT
+    kTypeSURF,          ///< SURF
 #endif
-        };
+  };
 
-        using KeyPoints = std::vector < cv::KeyPoint >;     ///< Keypoints
-        using Descriptors = cv::Mat;                        ///< Keypoint descriptors
+  using KeyPoints = std::vector<cv::KeyPoint>;  ///< Keypoints
+  using Descriptors = cv::Mat;                  ///< Keypoint descriptors
 
-        Feature ( );
-        Feature ( const cv::Mat_ < uchar > & image , Type type );
-        ~Feature ( );
-        Type GetType ( ) const { return type_; }
-        const KeyPoints & GetKeyPoints ( ) const { return key_points_; }
-        const Descriptors & GetDescriptors ( ) const { return descriptors_; }
+  Feature();
+  Feature(const cv::Mat_<uchar>& image, Type type);
+  ~Feature();
+  Type GetType() const { return type_; }
+  const KeyPoints& GetKeyPoints() const { return key_points_; }
+  const Descriptors& GetDescriptors() const { return descriptors_; }
 
-    private:
+ private:
+  Type type_;
+  KeyPoints key_points_;                   ///< Keypoints
+  Descriptors descriptors_;                ///< Keypoint descriptors
+  cv::Ptr<cv::FeatureDetector> detector_;  ///< Keypoint detector
+  friend class boost::serialization::access;
 
-        Type        type_;
-        KeyPoints   key_points_;        ///< Keypoints
-        Descriptors descriptors_;       ///< Keypoint descriptors
-        cv::Ptr<cv::FeatureDetector> detector_; ///< Keypoint detector
-        friend class boost::serialization::access;
+  BOOST_SERIALIZATION_SPLIT_MEMBER();
 
-        BOOST_SERIALIZATION_SPLIT_MEMBER ( );
+  template <class Archive>
+  void save(Archive& ar, const unsigned int version) const {
+    const cv::Mat m = descriptors_;
+    ar& type_;
+    ar& key_points_;
+    ar& m;
+  }
 
-        template < class Archive >
-        void save ( Archive & ar , const unsigned int version ) const {
-            const cv::Mat m = descriptors_;
-            ar & type_;
-            ar & key_points_;
-            ar & m;
-        }
+  template <class Archive>
+  void load(Archive& ar, const unsigned int version) {
+    cv::Mat m;
+    ar& type_;
+    ar& key_points_;
+    ar& m;
+    descriptors_ = m;
+  }
+};
 
-        template < class Archive >
-        void load ( Archive & ar , const unsigned int version ) {
-            cv::Mat m;
-            ar & type_;
-            ar & key_points_;
-            ar & m;
-            descriptors_ = m;
-        }
+bool SaveFeature(const std::string& name, const Feature& feature);
+bool LoadFeature(const std::string& name, Feature& feature);
 
-    };
+}  // namespace MapCreator
 
-    bool SaveFeature ( const std::string & name , const Feature & feature );
-    bool LoadFeature ( const std::string & name , Feature & feature );
-
-}
-
-
-#endif //MAPCREATOR_FEATURE_H
+#endif  // MAPCREATOR_FEATURE_H
--- src/Core/Feature.cpp
+++ src/Core/Feature.cpp
@@ -1,76 +1,77 @@
-//
-// Created by LinKun on 9/12/15.
-//
-
 #include "Core/Feature.h"
-
-#include <boost/iostreams/filtering_stream.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
-
 #include "Core/Logger.h"
+
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/filtering_stream.hpp>
 
 LOGGER("Core.Feature");
 
 namespace MapCreator {
 
-    Feature::Feature()
-            : type_(kTypeUnknown) { }
+Feature::Feature() : type_(kTypeUnknown) {}
 
-    Feature::Feature(const cv::Mat_<uchar> &image, Type type)
-            : type_(type) {
-
-        switch (type_) {
-            case kTypeORB: { detector_ = cv::ORB::create(); break; }
+Feature::Feature(const cv::Mat_<uchar> &image, Type type) : type_(type) {
+  switch (type_) {
+    case kTypeORB: {
+      detector_ = cv::ORB::create();
+      break;
+    }
 #ifdef ENABLE_OPENCV_CONTRIB
-            case kTypeFREAK: { detector_ = cv::xfeatures2d::FREAK::create(); break; }
-            case kTypeSIFT: { detector_ = cv::xfeatures2d::SIFT::create(); break; }
-            case kTypeSURF: { detector_ = cv::xfeatures2d::SURF::create(); break; }
+    case kTypeFREAK: {
+      detector_ = cv::xfeatures2d::FREAK::create();
+      break;
+    }
+    case kTypeSIFT: {
+      detector_ = cv::xfeatures2d::SIFT::create();
+      break;
+    }
+    case kTypeSURF: {
+      detector_ = cv::xfeatures2d::SURF::create();
+      break;
+    }
 #endif
-            default: break;
-        }
+    default:
+      break;
+  }
 
-        detector_->detectAndCompute(image, cv::Mat(), key_points_, descriptors_);
-    }
-
-    Feature::~Feature() { }
-
-    bool SaveFeature(const std::string &file_name, const Feature &feature) {
-
-        std::ofstream out(file_name, std::ios::binary);
-
-        if (out) {
-
-            namespace bio = boost::iostreams;
-
-            bio::filtering_ostream f;
-            f.push(bio::gzip_compressor());
-            f.push(out);
-
-            boost::archive::binary_oarchive ar(out);
-            ar << feature;
-            return true;
-        }
-        return false;
-    }
-
-    bool LoadFeature(const std::string &file_name, Feature &feature) {
-
-        std::ifstream in(file_name, std::ios::binary);
-
-        if (in) {
-
-            namespace bio = boost::iostreams;
-
-            bio::filtering_istream f;
-            f.push(bio::gzip_decompressor());
-            f.push(in);
-
-            boost::archive::binary_iarchive ar(in);
-            ar >> feature;
-            return true;
-        }
-
-        return false;
-    }
-
+  detector_->detectAndCompute(image, cv::Mat(), key_points_, descriptors_);
 }
+
+Feature::~Feature() {}
+
+bool SaveFeature(const std::string &file_name, const Feature &feature) {
+  std::ofstream out(file_name, std::ios::binary);
+
+  if (out) {
+    namespace bio = boost::iostreams;
+
+    bio::filtering_ostream f;
+    f.push(bio::gzip_compressor());
+    f.push(out);
+
+    boost::archive::binary_oarchive ar(out);
+    ar << feature;
+    return true;
+  }
+  return false;
+}
+
+bool LoadFeature(const std::string &file_name, Feature &feature) {
+  std::ifstream in(file_name, std::ios::binary);
+
+  if (in) {
+    namespace bio = boost::iostreams;
+
+    bio::filtering_istream f;
+    f.push(bio::gzip_decompressor());
+    f.push(in);
+
+    boost::archive::binary_iarchive ar(in);
+    ar >> feature;
+    return true;
+  }
+
+  return false;
+}
+
+}  // namespace MapCreator
