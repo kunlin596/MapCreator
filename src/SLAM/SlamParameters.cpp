87121034540772a697703dfb4bb6d46269193047
Kun Lin <kun.lin.596@gmail.com>
2019-05-14 14:17:01 +0900

    (refactor) Clean up SlamParameters.cpp

--- src/SLAM/SlamParameters.cpp
+++ src/SLAM/SlamParameters.cpp
@@ -4,14 +4,4 @@
 
 #include "SLAM/SlamParameters.h"
 
-namespace MapCreator {
-
-    template<>
-    TrackerParameters::Consecutive TrackerParameters::GetParameters() { return paramsConsectutive; }
-
-    template<>
-    TrackerParameters::FixedNumber TrackerParameters::GetParameters() { return paramsFixedNumber; }
-
-    template<>
-    TrackerParameters::KeyFrameOnly TrackerParameters::GetParameters() { return paramsKeyFramesOnly; }
-}
+namespace MapCreator {}
