80768b39d80c7b5e125b478a2f38237668e39004
Kun Lin <kun.lin.596@gmail.com>
2019-11-17 00:26:59 +0900

    (refactor) Refactor KeyFrame

--- include/SLAM/KeyFrame.h
+++ include/SLAM/KeyFrame.h
@@ -2,157 +2,169 @@
 // Created by LinKun on 10/6/15.
 //
 
-#ifndef MAPCREATOR_KEYFRAME_H
-#define MAPCREATOR_KEYFRAME_H
+#pragma once
 
-#include "Core/Serialize.h"
+#include <Eigen/Dense>
+
+#include "Core/Camera.h"
 #include "Core/Feature.h"
+#include "Core/PointCloud.h"
 
-#include "SLAM/Calibrator.h"
-#include "SLAM/SLAM.h"
-#include "Core/Frame.h"
-
-#include <QDir>
-#include <QFileInfo>
+namespace Eigen {
+using Matrix4f = Matrix<float, 4, 4>;
+}
 
 namespace MapCreator {
 
-	struct KeyFrame: public RGBDFrame
-	{
-	public:
+/**
+ * @brief      Class for key frame.
+ */
+class KeyFrame : public CameraBase {
+ public:
+  explicit KeyFrame(const std::string& name, const PointCloudXYZRGB& pointcloud,
+                    const Feature::Type& type = Feature::Type::ORB)
+      : name_(name), type_(type), is_used_(false) {}
 
-		KeyFrame ( const std::string & name , const PointImage & pointimage , const ColorImage & colorimage ,
-		           const Feature::Type & type = Feature::Type::kTypeORB)
-		{
-			name_ = name;
-			this->pointimage = pointimage;
-			this->colorimage = colorimage;
-			this->type_ = type;
-			this->is_used_ = false;
-			CreateFeature ( );
-		}
+  KeyFrame() = delete;
 
-		KeyFrame ( ) = default;
-		~KeyFrame ( ) = default;
+  ~KeyFrame() = default;
 
-		// Setters
-		void SetId ( const int & id ) { id_ = id; }
-		void SetName ( const std::string & name ) { name_ = name; }
-		void SetColorImage ( const ColorImage & colorimage , const Feature::Type & type = Feature::Type::kTypeORB ) {
+  /**
+   * @brief      Gets the identifier.
+   *
+   * @return     The identifier.
+   */
+  int GetId() const noexcept { return id_; }
 
-			type_        = type;
-			this->colorimage = colorimage;
-			CreateFeature ( );
-		}
-		void SetPointImage ( const PointImage & pointimage ) { this->pointimage = pointimage; }
-		void SetAlignmentMatrix ( const glm::mat4 & mat ) { alignment_matrix_ = mat; }
-		void SetAnswerAlignmentMatrix ( const glm::mat4 & mat ) { marker_alignment_matrix_ = mat; }
-		void SetUsed ( bool is_used ) { is_used_ = is_used; }
+  /**
+   * @brief      Gets the point cloud.
+   *
+   * @return     The point cloud.
+   */
+  const PointCloudXYZRGB& GetPointCloud() const { return pointCloud_; }
 
-		// Getters
-		int GetId ( ) const { return id_; }
-		const ColorImage & GetColorImage ( ) const { return colorimage; }
-		const PointImage & GetPointImage ( ) const { return pointimage; }
-		const std::string & GetName ( ) const { return name_; }
-		const MapCreator::Feature & GetFeature ( ) const { return feature_; }
-		const MapCreator::Feature::Type & GetFeatureType ( ) const { return type_; }
-		const glm::mat4 & GetAlignmentMatrix ( ) const { return alignment_matrix_; }
-		const glm::mat4 & GetAnswerAlignmentMatrix ( ) const { return marker_alignment_matrix_; }
-		const bool IsUsed ( ) const { return is_used_; }
+  /**
+   * @brief      Gets the name.
+   *
+   * @return     The name.
+   */
+  const std::string& GetName() const noexcept { return name_; }
 
-	private: // Private methods
+  /**
+   * @brief      Gets the feature.
+   *
+   * @return     The feature.
+   */
+  const Feature& GetFeature() const noexcept { return feature_; }
 
-		// Boost serialization methods
-		friend class boost::serialization::access;
+  /**
+   * @brief      Gets the feature type.
+   *
+   * @return     The feature type.
+   */
+  const Feature::Type& GetFeatureType() const noexcept { return type_; }
 
-		BOOST_SERIALIZATION_SPLIT_MEMBER ( );
-		template < class Archive > void save ( Archive & ar , const unsigned int version ) const {
+  /**
+   * @brief      Sets the is used.
+   *
+   * @param[in]  is_used  Indicates if used
+   */
+  void SetIsUsed(bool is_used) { is_used_ = is_used; }
 
-			const cv::Mat color = colorimage;
-			const cv::Mat point = pointimage;
+  /**
+   * @brief      Determines if used.
+   *
+   * @return     True if used, False otherwise.
+   */
+  bool IsUsed() const noexcept { return is_used_; }
 
-			ar & color;
-			ar & point;
-			ar & name_;
-			ar & feature_;
+ private:
+  int id_;  ///< Unique id of the key frame
 
-		}
-		template < class Archive > void load ( Archive & ar , const unsigned int version ) {
+  std::string name_;
+  Feature::Type type_;
+  bool is_used_;
 
-			cv::Mat     color;
-			cv::Mat     point;
-			std::string name;
-			Feature     feature;
+  Feature feature_;
+  PointCloudXYZRGB pointCloud_;
 
-			ar & color;
-			ar & point;
-			ar & name;
-			ar & feature;
+  // Boost serialization methods
+  // friend class boost::serialization::access;
 
-			colorimage = color;
-			pointimage = point;
-			name_        = name;
-			feature_     = feature;
-		}
+  // BOOST_SERIALIZATION_SPLIT_MEMBER();
+  // template <class Archive>
+  // void save(Archive& ar, const unsigned int version) const {
+  //   const cv::Mat color = colorimage;
+  //   const cv::Mat point = pointimage;
 
-		inline void CreateFeature ( ) {
+  //   ar& color;
+  //   ar& point;
+  //   ar& name_;
+  //   ar& feature_;
+  // }
+  // template <class Archive>
+  // void load(Archive& ar, const unsigned int version) {
+  //   cv::Mat color;
+  //   cv::Mat point;
+  //   std::string name;
+  //   Feature feature;
 
-			QFileInfo info ( QString::fromStdString ( name_ ) );
+  //   ar& color;
+  //   ar& point;
+  //   ar& name;
+  //   ar& feature;
 
-			auto path = info.absolutePath ( );
-			auto name = info.completeBaseName ( );
+  //   colorimage = color;
+  //   pointimage = point;
+  //   name_ = name;
+  //   feature_ = feature;
+  // }
 
+  // inline void CreateFeature() {
+  //     QFileInfo info(QString::fromStdString(name_));
 
-			QString feature_prefix;
-			switch ( type_ ) {
-				case Feature::Type::kTypeORB:
-					feature_prefix = "ORB/orb_";
-					break;
-#ifdef ENABLE_OPENCV_CONTRIB
-				case Feature::Type::kTypeSIFT:
-					feature_prefix = "SIFT/sift_";
-					break;
-				case Feature::Type::kTypeSURF:
-					feature_prefix = "SURF/surf_";
-					break;
-				case Feature::Type::kTypeFREAK:
-					feature_prefix = "FREAK/freak_";
-					break;
-#endif
-				default:
-					break;
-			}
+  //     auto path = info.absolutePath();
+  //     auto name = info.completeBaseName();
 
-			QString feature_folder_path = path + "/Features/";
+  //     // QString feature_prefix;
+  //     switch (type_) {
+  //       case Feature::Type::kTypeORB:
+  //         feature_prefix = "ORB/orb_";
+  //         break;
+  // #ifdef ENABLE_OPENCV_CONTRIB
+  //       case Feature::Type::kTypeSIFT:
+  //         feature_prefix = "SIFT/sift_";
+  //         break;
+  //       case Feature::Type::kTypeSURF:
+  //         feature_prefix = "SURF/surf_";
+  //         break;
+  //       case Feature::Type::kTypeFREAK:
+  //         feature_prefix = "FREAK/freak_";
+  //         break;
+  // #endif
+  //       default:
+  //         break;
+  //     }
 
-			QDir dir ( feature_folder_path );
-			if ( !dir.exists ( ) ) dir.mkdir ( feature_folder_path );
+  //   QString feature_folder_path = path + "/Features/";
 
-			if ( !LoadFeature ( ( feature_folder_path + name + "." + "feature" ).toStdString ( ) , feature_ ) ) {
-				assert( !colorimage.empty ( ) );
-				cv::Mat cvt_color_image;
-				cv::cvtColor ( colorimage , cvt_color_image , cv::COLOR_RGB2GRAY );
-				feature_ = MapCreator::Feature ( cvt_color_image , type_ );
-				SaveFeature ( QString ( feature_folder_path + name + ".feature" ).toStdString ( ) , feature_ );
-			}
-		}
+  //   QDir dir(feature_folder_path);
+  //   if (!dir.exists()) dir.mkdir(feature_folder_path);
 
-	private: // Fields
+  //   if (!LoadFeature(
+  //           (feature_folder_path + name + "." + "feature").toStdString(),
+  //           feature_)) {
+  //     assert(!colorimage.empty());
+  //     cv::Mat cvt_color_image;
+  //     cv::cvtColor(colorimage, cvt_color_image, cv::COLOR_RGB2GRAY);
+  //     feature_ = Feature(cvt_color_image, type_);
+  //     SaveFeature(
+  //         QString(feature_folder_path + name + ".feature").toStdString(),
+  //         feature_);
+  //   }
+  // }
+};
 
-		int                id_;
-		bool               is_used_;
-		glm::mat4          marker_alignment_matrix_;
-		glm::mat4          alignment_matrix_;
-		std::string        name_;
-		MapCreator::Feature feature_;
-		MapCreator::Feature::Type type_;
+using KeyFrames = std::vector<KeyFrame>;
 
-		PointImage pointimage;
-
-	};
-
-	using KeyFrames = std::vector < KeyFrame >;
-
-}
-
-#endif //MAPCREATOR_KEYFRAME_H
+};  // namespace MapCreator
