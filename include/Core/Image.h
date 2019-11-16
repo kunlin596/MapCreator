c63d930f5ec713b9c85986054f4fb201b2d6465b
Kun Lin <kun.lin.596@gmail.com>
2019-11-16 16:01:46 +0900

    (doc) Document Image.h/.cpp

--- include/Core/Image.h
+++ include/Core/Image.h
@@ -8,55 +8,99 @@
 #include <opencv2/opencv.hpp>
 
 namespace MapCreator {
-    /// 画像のサイズ変更
-    template < typename T >
-    cv::Mat_ < T > Resize ( const cv::Mat_ < T > & image , int w , int h , int interpolation = cv::INTER_LINEAR ) {
 
-        cv::Mat_ < T > cvt_image;
+/**
+ * @brief      Resice the image
+ *
+ * @param[in]  image          The image
+ * @param[in]  w              { parameter_description }
+ * @param[in]  h              { parameter_description }
+ * @param[in]  interpolation  The interpolation
+ *
+ * @tparam     T              { description }
+ *
+ * @return     { description_of_the_return_value }
+ */
+template <typename T>
+cv::Mat_<T> Resize(const cv::Mat_<T>& image, int w, int h,
+                   int interpolation = cv::INTER_LINEAR) {
+  cv::Mat_<T> cvt_image;
 
-        if ( !image.empty ( ) && w > 0 && h > 0 ) {
-            cv::resize ( image , cvt_image , cv::Size ( w , h ) , 0 , 0 , interpolation );
-        }
+  if (!image.empty() && w > 0 && h > 0) {
+    cv::resize(image, cvt_image, cv::Size(w, h), 0, 0, interpolation);
+  }
 
-        return cvt_image;
-    }
-
-
-    /// 画像の反転
-    template < typename T >
-    cv::Mat_ < T > Flip ( const cv::Mat_ < T > & image , int code ) {
-
-        cv::Mat_ < T > cvt_image;
-        cv::flip ( image , cvt_image , code );
-        return cvt_image;
-    }
-
-
-    /// 画像の転置
-    template < typename T >
-    cv::Mat_ < T > Transpose ( const cv::Mat_ < T > & image ) {
-
-        cv::Mat_ < T > cvt_image;
-        cv::transpose ( image , cvt_image );
-        return cvt_image;
-    }
-
-
-    /// RGB→BGR（またはその逆）
-    cv::Mat_ < cv::Vec3b > SwapChannel ( const cv::Mat_ < cv::Vec3b > & image );
-
-
-    /// RGB 画像(24bit)をグレースケール(8bit)に変換する
-    cv::Mat_ < uchar > RGBToGray ( const cv::Mat_ < cv::Vec3b > & image );
-
-
-    /// BGR 画像(24bit)をグレースケール(8bit)に変換する
-    cv::Mat_ < uchar > BGRToGray ( const cv::Mat_ < cv::Vec3b > & image );
-
-
-    /// グレー画像をカラー画像にする
-    cv::Mat_ < cv::Vec3b > GrayToColor ( const cv::Mat_ < uchar > & image );
+  return cvt_image;
 }
 
+/**
+ * @brief      Flip image
+ *
+ * @param[in]  image  The image
+ * @param[in]  code   The code
+ *
+ * @tparam     T      { description }
+ *
+ * @return     { description_of_the_return_value }
+ */
+template <typename T>
+cv::Mat_<T> Flip(const cv::Mat_<T>& image, int code) {
+  cv::Mat_<T> cvt_image;
+  cv::flip(image, cvt_image, code);
+  return cvt_image;
+}
+
+/**
+ * @brief      Transpose the image
+ *
+ * @param[in]  image  The image
+ *
+ * @tparam     T      { description }
+ *
+ * @return     { description_of_the_return_value }
+ */
+template <typename T>
+cv::Mat_<T> Transpose(const cv::Mat_<T>& image) {
+  cv::Mat_<T> cvt_image;
+  cv::transpose(image, cvt_image);
+  return cvt_image;
+}
+
+/**
+ * @brief      Swap channels
+ *
+ * @param[in]  image  The image
+ *
+ * @return     { description_of_the_return_value }
+ */
+cv::Mat_<cv::Vec3b> SwapChannel(const cv::Mat_<cv::Vec3b>& image);
+
+/**
+ * @brief      Convert RGB to gray image
+ *
+ * @param[in]  image  The image
+ *
+ * @return     { description_of_the_return_value }
+ */
+cv::Mat_<uchar> RGBToGray(const cv::Mat_<cv::Vec3b>& image);
+
+/**
+ * @brief      Convert color image to
+ *
+ * @param[in]  image  The image
+ *
+ * @return     { description_of_the_return_value }
+ */
+cv::Mat_<uchar> BGRToGray(const cv::Mat_<cv::Vec3b>& image);
+
+/**
+ * @brief      Convert gray scale image to RGB image
+ *
+ * @param[in]  image  The image
+ *
+ * @return     { description_of_the_return_value }
+ */
+cv::Mat_<cv::Vec3b> GrayToColor(const cv::Mat_<uchar>& image);
+}  // namespace MapCreator
 
 #endif //MAPCREATOR_IMAGE_H
--- src/Core/Image.cpp
+++ src/Core/Image.cpp
@@ -6,34 +6,27 @@
 
 namespace MapCreator {
 
-    // RGB→BGR（またはその逆）
-    cv::Mat_<cv::Vec3b> SwapChannel(const cv::Mat_<cv::Vec3b> &image) {
-        cv::Mat_<cv::Vec3b> cvt_image;
-        cv::cvtColor(image, cvt_image, cv::COLOR_RGB2BGR);
-        return cvt_image;
-    }
+cv::Mat_<cv::Vec3b> SwapChannel(const cv::Mat_<cv::Vec3b> &image) {
+  cv::Mat_<cv::Vec3b> cvt_image;
+  cv::cvtColor(image, cvt_image, cv::COLOR_RGB2BGR);
+  return cvt_image;
+}
 
+cv::Mat_<uchar> RGBToGray(const cv::Mat_<cv::Vec3b> &image) {
+  cv::Mat_<uchar> cvt_image;
+  cv::cvtColor(image, cvt_image, cv::COLOR_RGB2GRAY);
+  return cvt_image;
+}
 
-    // RGB 画像(24bit)をグレースケール(8bit)に変換する
-    cv::Mat_<uchar> RGBToGray(const cv::Mat_<cv::Vec3b> &image) {
-        cv::Mat_<uchar> cvt_image;
-        cv::cvtColor(image, cvt_image, cv::COLOR_RGB2GRAY);
-        return cvt_image;
-    }
+cv::Mat_<uchar> BGRToGray(const cv::Mat_<cv::Vec3b> &image) {
+  cv::Mat_<uchar> cvt_image;
+  cv::cvtColor(image, cvt_image, cv::COLOR_BGR2GRAY);
+  return cvt_image;
+}
 
-
-    // BGR 画像(24bit)をグレースケール(8bit)に変換する
-    cv::Mat_<uchar> BGRToGray(const cv::Mat_<cv::Vec3b> &image) {
-        cv::Mat_<uchar> cvt_image;
-        cv::cvtColor(image, cvt_image, cv::COLOR_BGR2GRAY);
-        return cvt_image;
-    }
-
-
-    // グレースケール(8bit)をカラー画像(24bit)に変換する
-    cv::Mat_<cv::Vec3b> GrayToColor(const cv::Mat_<uchar> &image) {
-        cv::Mat_<cv::Vec3b> cvt_image;
-        cv::cvtColor(image, cvt_image, cv::COLOR_GRAY2BGR);
-        return cvt_image;
-    }
-}    // a
+cv::Mat_<cv::Vec3b> GrayToColor(const cv::Mat_<uchar> &image) {
+  cv::Mat_<cv::Vec3b> cvt_image;
+  cv::cvtColor(image, cvt_image, cv::COLOR_GRAY2BGR);
+  return cvt_image;
+}
+}  // namespace MapCreator
